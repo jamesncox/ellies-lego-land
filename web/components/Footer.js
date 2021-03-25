840babc36bf6132aeb7f7754914eb8857abd9a6d
jamesncox <jamesncox@yahoo.com>
2021-03-25 13:36:55 -0500

    Update Footer with link to portfolio and hover yellow

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -1,7 +1,17 @@
 export default function Footer() {
   return (
     <div className="text-center border-t border-blue-800 mx-5 mt-16 py-5">
-      <p className="text-blue-900">Made with ❤️‍ by James Cox</p>
+      <p className="text-blue-900">
+        Made with ❤️‍ by{" "}
+        <a
+          className="font-bold hover:text-yellow-300"
+          href="https://www.jamescox.dev/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          James Cox
+        </a>
+      </p>
       <p className="text-blue-900">
         {"Copyright © "}
         {new Date().getFullYear()}
