743909577be268f549fc93020a25b060b33b46a4
jamesncox <jamesncox@yahoo.com>
2021-03-25 12:06:55 -0500

    Update Footer.js styles/colors

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <div className="text-center border-t border-blue-800 mx-5 py-5">
-      <p>Made with ❤️‍ by James Cox</p>
-      <p>
+      <p className="text-blue-900">Made with ❤️‍ by James Cox</p>
+      <p className="text-blue-900">
         {"Copyright © "}
         {new Date().getFullYear()}
       </p>
