dc532be00885485f49b3bf2111a3f9d88072fa38
jamesncox <jamesncox@yahoo.com>
2021-03-25 11:58:31 -0500

    Update Footer styles

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="footer">
+    <div className="text-center border-t border-blue-800 mx-5 py-5">
       <p>Made with ❤️‍ by James Cox</p>
       <p>
         {"Copyright © "}
