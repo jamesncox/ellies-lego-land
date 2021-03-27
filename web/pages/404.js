85d8c1615508e7fc3b2d8590fd57579e22cd373d
jamesncox <jamesncox@yahoo.com>
2021-03-27 12:30:17 -0500

    Update 404.js with responsive styles

--- web/pages/404.js
+++ web/pages/404.js
@@ -1,7 +1,19 @@
 export default function Custom404() {
   return (
-    <div className="flex h-screen bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 ">
-      <h1 className="m-auto mt-16 text-xl font-mono">404 - Page Not Found</h1>
+    <div className="flex flex-col md:flex-row h-screen bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 ">
+      <div className="flex md:flex-col mx-auto mt-5 md:mt-64 md:mx-auto">
+        <h1 className="text-3xl mr-5 md:text-8xl font-bold font-mono text-white opacity-80">
+          404
+        </h1>
+        <h1 className="text-3xl md:text-4xl font-bold font-mono text-white opacity-80">
+          Page Not Found
+        </h1>
+      </div>
+      <img
+        className="h-auto md:h-96 mx-5 md:h-4/5 mt-5 md:mt-8"
+        src="/images/emmet-lego.png"
+        alt="Emmet from Lego Movie running and screaming"
+      />
     </div>
   );
 }
