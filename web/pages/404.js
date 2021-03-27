925b727188612e3a2d8910b794e0b79f874d0749
jamesncox <jamesncox@yahoo.com>
2021-03-27 11:12:25 -0500

    Add emmet-lego.png to public/images

---
+++ web/pages/404.js
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <div className="flex h-screen bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 ">
+      <h1 className="m-auto mt-16 text-xl font-mono">404 - Page Not Found</h1>
+    </div>
+  );
+}
