760c84c94ff873ee27c89763d5864123dc048eac
jamesncox <jamesncox@yahoo.com>
2021-03-27 12:33:44 -0500

    Update 404.js margin-top: 10

--- web/pages/404.js
+++ web/pages/404.js
@@ -1,7 +1,7 @@
 export default function Custom404() {
   return (
     <div className="flex flex-col md:flex-row h-screen bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 ">
-      <div className="flex md:flex-col mx-auto mt-5 md:mt-64 md:mx-auto">
+      <div className="flex md:flex-col mx-auto mt-10 md:mt-64 md:mx-auto">
         <h1 className="text-3xl mr-5 md:text-8xl font-bold font-mono text-white opacity-80">
           404
         </h1>
